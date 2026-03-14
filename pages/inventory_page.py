101e552c715fa71960d66579d7507596dd552042
Ishan <ishuk2000@gmail.com>
2026-03-14 18:07:42 +0530

    Add InventoryPage interactions: add backpack to cart and verify cart badge using Playwright expect

--- pages/inventory_page.py
+++ pages/inventory_page.py
@@ -4,11 +4,21 @@ from playwright.sync_api import sync_playwright,expect
 
 class InventoryPage(BasePage):
     PRODUCT_TITLE='.title'
+    BACKPACK_ADD_TO_CARTS='#add-to-cart-sauce-labs-backpack'
+    CART_BADGE="[data-test='shopping-cart-badge']"
 
     def __init__(self,page):
         super().__init__(page)
     
     def verify_products_page(self):
         expect(self.page.locator(self.PRODUCT_TITLE)).to_have_text("Products")
+
+    def add_to_cart_backpack(self):
+        self.click(self.BACKPACK_ADD_TO_CARTS)
+
+    def verify_cart_badge(self):
+        expect(self.page.locator(self.CART_BADGE)).to_have_text("1")
+    
+
         
         
--- tests/test_login.py
+++ tests/test_login.py
@@ -5,4 +5,6 @@ def test_login(page):
      login_page=LoginPage(page)
      inventory_page=login_page.login("standard_user","secret_sauce")
      inventory_page.verify_products_page()
+     inventory_page.add_to_cart_backpack()
+     inventory_page.verify_cart_badge()
      
